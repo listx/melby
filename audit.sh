66471adc52edace0e4186a16f716054cc5a38112
Linus Arver <linus@ucla.edu>
2024-07-20 12:23:59 +1000

    add script to audit tangled files
    Although we use LP, sometimes there are cases where it's easier to just
    create and track the file once, separately. For example, LICENSE text.
    
    In the future we should check that the differences presented by the
    audit script are all accounted for (and understood).

---
+++ audit.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_ROOT="$(dirname "$(realpath "$0")")"
+
+tangled_files()
+{
+    git -C "${SCRIPT_ROOT}" grep -P '^#\+begin_\w+ \w+ :tangle' -- '*.org' \
+        | sed 's/^.\+:tangle \(\S\+\).*$/\1/' \
+        | sort
+}
+
+tracked_files()
+{
+    git -C "${SCRIPT_ROOT}" ls-tree -r HEAD \
+        | awk '{print $2,$4}' \
+        | grep '^blob' \
+        | grep -v '\.org$' \
+        | awk '{print $2}'
+}
+
+main()
+{
+    diff -u <(tangled_files) <(tracked_files)
+}
+
+main "$@"
